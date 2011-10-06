f7ce46d815883e9d0f78454716e62645d1856673
Victor Manuel De Ponte Olivares <rdbvictor19@gmail.com>
2011-10-05 23:57:58 -0430

    Lo que llevamos hasta ahora. =(

--- src/videorent/acciones/BuscarArticulo.java
+++ src/videorent/acciones/BuscarArticulo.java
@@ -11,6 +11,27 @@ package videorent.acciones;
  * @author <a href="http://www.usb.ve/">Universidad Simón Bolívar</a>
  * @version
  */
-public class BuscarArticulo {
+public class BuscarArticulo extends AccionEmpleado {
+    private String codCliente;
+    private String codArtículo;
 
+    /**
+     * Construye una nueva acción del tipo BuscarArticulo, en base a la
+     * información suministrada.
+     * @param codCliente código del cliente que ordenó la búsqueda
+     * @param codArtículo código del artículo a buscar.
+     */
+    public BuscarArticulo(String codCliente, String codArtículo) {
+        super('b');
+        this.codCliente = codCliente;
+        this.codArtículo = codArtículo;
+    }
+
+    public String getCodArtículo() {
+        return codArtículo;
+    }
+
+    public String getCodCliente() {
+        return codCliente;
+    }
 }
--- src/videorent/acciones/Facturar.java
+++ src/videorent/acciones/Facturar.java
@@ -11,6 +11,36 @@ package videorent.acciones;
  * @author <a href="http://www.usb.ve/">Universidad Simón Bolívar</a>
  * @version
  */
-public class Facturar {
+public class Facturar extends AccionEmpleado {
+    private String codCliente;
+    private double motoCobrado;
 
+    /**
+     * Construye una nueva acción del tipo Facturar según la información
+     * suministrada.
+     *
+     * @param codCliente código del cliente a facturar.
+     * @param motoCobrado monto cobrado al cliente
+     */
+    public Facturar(String codCliente, double motoCobrado) {
+        super('f');
+        this.codCliente = codCliente;
+        this.motoCobrado = motoCobrado;
+    }
+
+    /**
+     * Devuelve el código del cliente a facturar.
+     * @return el código del cliente a facturar.
+     */
+    public String getCodCliente() {
+        return codCliente;
+    }
+
+    /**
+     * Devuelve el moto cobrado en esta {@code Facturación}
+     * @return el moto cobrado en esta {@code Facturación}
+     */
+    public double getMotoCobrado() {
+        return motoCobrado;
+    }
 }
