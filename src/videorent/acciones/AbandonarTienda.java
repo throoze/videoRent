5da11fb4611a051d0a1e22a3929b0a0e27dae079
Victor Manuel De Ponte Olivares <rdbvictor19@gmail.com>
2011-10-06 10:58:00 -0430

    Acciones de los no asociados listas

--- src/videorent/acciones/AbandonarTienda.java
+++ src/videorent/acciones/AbandonarTienda.java
@@ -1,5 +1,8 @@
 package videorent.acciones;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Representa la acción de abandonar la tienda realizada por un cliente.
  *
@@ -10,22 +13,45 @@ package videorent.acciones;
  */
 public class AbandonarTienda extends AccionCliente{
     private String codCliente;
+    private String cedula;
 
     /**
-     * Constructor de la clase AbandonarTienda
+     * Constructor de la clase AbandonarTienda. Diferencia entre si lo hace un
+     * cliente asociado o uno no asociado.
+     *
      * @param codCliente El código del cliente
      * @param id El código de la acción
      */
     public AbandonarTienda(String codCliente) {
         super('b');
-        this.codCliente = codCliente;
+        Pattern p = Pattern.compile("^[BP]d{4}$");
+        Matcher m = p.matcher(codCliente);
+        if (m.matches()) {
+            this.codCliente = codCliente;
+            this.cedula = null;
+        } else {
+            this.cedula = codCliente;
+            this.codCliente = null;
+        }
     }
 
     /**
-     * Devuelve el atributo codCliente
+     * Devuelve el código del cliente que abandona la tienda en caso de ser un
+     * cliente asociado. En caso de no estar asociado, devuelve {@code null}.
      * @return El código del cliente
      */
     public String getCodCliente() {
         return codCliente;
+    }
+
+    /**
+     * Devuelve la cédula del cliente que abandona la tienda, en caso de que no
+     * esté asociado. Si se trata de un cliente asociado, devuelve {@code null}.
+     *
+     * @return la cédula del cliente que abandona la tienda, en caso de que no
+     * esté asociado. Si se trata de un cliente asociado, devuelve {@code null}.
+     */
+    public String getCedula() {
+        return cedula;
     }
 }
